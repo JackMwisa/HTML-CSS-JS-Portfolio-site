708bc671d7cf093b8239532d09031d660003bfb3
JackMwisa <mukewajack@gmail.com>
2022-07-26 14:28:04 +0300

    app js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls')
+const sectBtn = document.querySelectorAll('.control') 
+const allSections = document.querySelector('.main-content')
+
+function pageTransistions(){
+    //Button click active class
+
+    for (let i=0, i < sectBtn.length; i++){
+        
+        sectBtn[i].addEventListener('click', )
+    }
+
+}
