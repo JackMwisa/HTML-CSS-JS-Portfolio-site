4931d9d56d548470e75c688cc3906670e2c8cf43
JackMwisa <mukewajack@gmail.com>
2022-07-26 14:48:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ function pageTransistions(){
 
     for (let i=0, i < sectBtn.length; i++){
         
-        sectBtn[i].addEventListener('click', )
+        sectBtn[i].addEventListener('click', () = {
+            
+            let currentBtn = document.querySelectorAll('.active-btn'),
+            currentBtn[0].classList = currentBtn[0].className,            
+        })
     }
 
 }
